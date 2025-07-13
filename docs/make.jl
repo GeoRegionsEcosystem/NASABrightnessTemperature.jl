221a22e06b6a19ac31ce2714ff9f34e914c92b95
Nathanael Wong <natgeo.wong@outlook.com>
2025-07-12 22:31:47 -0400

    Adding documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,77 @@
-using NASABrightnessTemperature
 using Documenter
+using DocumenterVitepress
+using NASAMergedTb
+import CairoMakie
 
-DocMeta.setdocmeta!(NASABrightnessTemperature, :DocTestSetup, :(using NASABrightnessTemperature); recursive=true)
+CairoMakie.activate!(type = "svg")
+
+DocMeta.setdocmeta!(NASAMergedTb, :DocTestSetup, :(using GeoRegions); recursive=true)
 
 makedocs(;
-    modules=[NASABrightnessTemperature],
-    authors="Nathanael Wong <natgeo.wong@outlook.com>",
-    sitename="NASABrightnessTemperature.jl",
-    format=Documenter.HTML(;
-        canonical="https://natgeo-wong.github.io/NASABrightnessTemperature.jl",
-        edit_link="main",
-        assets=String[],
+    modules  = [NASAMergedTb],
+    authors  = "Nathanael Wong <natgeo.wong@outlook.com>",
+    sitename = "NASAMergedTb.jl",
+    doctest  = false,
+    warnonly = true,
+    format   = DocumenterVitepress.MarkdownVitepress(
+        repo = "https://github.com/GeoRegionsEcosystem/NASAMergedTb.jl",
     ),
     pages=[
-        "Home" => "index.md",
+        "Home"                 => "index.md"
+        # "What is a GeoRegion?" => "georegions.md",
+        # "Basics"               => [
+        #     "Load/Read a GeoRegion"     => "basics/read.md",
+        #     "Creating a GeoRegion"      => "basics/create.md",
+        #     "Shape of a GeoRegion"      => "basics/shape.md",
+        #     "List Available GeoRegions" => "basics/tables.md",
+        #     "Predefined GeoRegions"     => [
+        #         "Predefined Datasets" => "basics/predefined/datasets.md",
+        #         "List All GeoRegions" => "basics/predefined/listall.md",
+        #     ],
+        # ],
+        # "Tutorials"            => [
+        #     "Using GeoRegions.jl"          => [
+        #         "Is it in a GeoRegion?"        => "tutorials/using/isin.md",
+        #         "Is it on a GeoRegion?"        => "tutorials/using/ison.md",
+        #         "Equivalence in GeoRegions.jl" => "tutorials/using/isequal.md",
+        #         "Derotation of Coordinates"    => "tutorials/using/derotate.md",
+        #     ],
+        #     "GeoRegions.jl for Projects"   => [
+        #         "Setting Up"        => "tutorials/projects/setup.md",
+        #         "Add, Read, Remove" => "tutorials/projects/addreadrm.md",
+        #         "Backends"          => "tutorials/projects/backends.md",
+        #     ],
+        # ],
+        # "API"                  => [
+        #     "Create, Add, Read, Remove" => "api/createaddreadrm.md",
+        #     "Project Setup"             => "api/project.md",
+        #     "Tables"                    => "api/tables.md",
+        #     "Shape / Coordinates"       => "api/shape.md",
+        #     "Derotate"                  => "api/derotation.md",
+        #     "Is In/On/Equal?"           => "api/isinonequal.md",
+        # ],
+        # "Ecosystem"            => "ecosystem.md",
     ],
 )
 
-deploydocs(;
-    repo="github.com/natgeo-wong/NASABrightnessTemperature.jl",
-    devbranch="main",
+recursive_find(directory, pattern) =
+    mapreduce(vcat, walkdir(directory)) do (root, dirs, files)
+        joinpath.(root, filter(contains(pattern), files))
+    end
+
+files = []
+for pattern in [r"\.cst", r"\.nc", "test.geo"]
+    global files = vcat(files, recursive_find(@__DIR__, pattern))
+end
+
+for file in files
+    rm(file)
+end
+
+DocumenterVitepress.deploydocs(;
+    repo      = "github.com/GeoRegionsEcosystem/NASAMergedTb.jl.git",
+    target    = "build", # this is where Vitepress stores its output
+    devbranch = "main",
+    branch    = "gh-pages",
+    push_preview = true,
 )
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,14 +1,59 @@
-```@meta
-CurrentModule = NASABrightnessTemperature
+```@raw html
+---
+layout: home
+
+hero:
+  name: "NASAMergedTb.jl"
+  text: "Gridded Brightness Temperature for Clouds"
+  tagline: Handling NASA's NCEP/CPC Level 3 Merged Infrared Brightness Temperature Dataset in Julia
+  image:
+    src: /logo.png
+    alt: NASAMergedTb
+  actions:
+    - theme: brand
+      text: Getting Started
+      link: /basics
+    - theme: alt
+      text: Datasets
+      link: /datasets
+    - theme: alt
+      text: Tutorials
+      link: /tutorials
+    - theme: alt
+      text: View on Github
+      link: https://github.com/GeoRegionsEcosystem/NASAMergedTb.jl
+      
+
+features:
+  - title: 🔍 Simple and Intuitive
+    details: NASAMergedTb aims to be simple and intuitive to the user, with basic functions like `download()` and `read()`.
+  - title: 🌍 Region of Interest
+    details: You don't have to download the global dataset, only for your (Geo)Region of interest, saving you time and disk space for small domains.
+  - title: 🏔️ Analysis
+    details: With NASAMergedTb, find regions of Shallow and Deep Convection/Clouds for your analysis based on your thresholds.
+---
+
+
+---
 ```
 
-# NASABrightnessTemperature
+## Installation Instructions
 
-Documentation for [NASABrightnessTemperature](https://github.com/natgeo-wong/NASABrightnessTemperature.jl).
-
-```@index
+The latest version of NASAMergedTb can be installed using the Julia package manager (accessed by pressing `]` in the Julia command prompt)
+```julia-repl
+julia> ]
+(@v1.10) pkg> add NASAMergedTb
 ```
 
-```@autodocs
-Modules = [NASABrightnessTemperature]
+You can update `NASAMergedTb.jl` to the latest version using
+```julia-repl
+(@v1.10) pkg> update NASAMergedTb
 ```
+
+And if you want to get the latest release without waiting for me to update the Julia Registry (although this generally isn't necessary since I make a point to release patch versions as soon as I find bugs or add new working features), you may fix the version to the `main` branch of the GitHub repository:
+```julia-repl
+(@v1.10) pkg> add NASAMergedTb#main
+```
+
+## Getting help
+If you are interested in using `NASAMergedTb.jl` or are trying to figure out how to use it, please feel free to ask me questions and get in touch!  Please feel free to [open an issue](https://github.com/GeoRegionsEcosystem/NASAMergedTb.jl/issues/new) if you have any questions, comments, suggestions, etc!
